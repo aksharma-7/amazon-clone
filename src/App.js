b58a44c4f789be3c31be9c62e22bdc7f590d2730
aksharma-7 <52443790+aksharma-7@users.noreply.github.com>
2021-03-26 14:28:36 +0530

    added react router dom

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
+import { BrowserRouter as Router, Link, Switch, Route } from 'react-router-dom';
 import './App.css';
 import Header from './Components/Header';
+import Cart from './Views/Cart';
+import Home from './Views/Home';
 
 function App() {
   return (
-    <div className='App'>
-      <Header />
-    </div>
+    <Router>
+      <div className='App'>
+        <Header />
+
+        <Switch>
+          <Route path='/cart'>
+            <Cart />
+          </Route>
+
+          <Route path='/'>
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
